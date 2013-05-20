b9f58a0a9d5bd9656f51051de23324e68450782a
S Ravi Kiran <yuvakiran2009@gmail.com>
2013-05-21 01:07:06 +0530

    Uploading required code

---
+++ AngularJSImplementation/controller.js
@@ -0,0 +1,37 @@
+﻿var StockTickerCtrl = function ($scope, stockTickerData) {
+
+    $scope.stocks = [];
+    $scope.marketIsOpen = false;
+
+    $scope.openMarket = function () {
+        ops.openMarket();
+    }
+
+    $scope.closeMarket = function () {
+        ops.closeMarket();
+    }
+
+    $scope.reset = function () {
+        ops.reset();
+    }
+
+    function assignStocks(stocks) {
+        $scope.stocks = stocks;
+    }
+
+    function replaceStock(stock) {
+        for (var count = 0; count < $scope.stocks.length; count++) {
+            if ($scope.stocks[count].Symbol == stock.Symbol) {
+                $scope.stocks[count] = stock;
+            }
+        }
+    }
+
+    function setMarketState(isOpen) {
+        $scope.marketIsOpen = isOpen;
+    }
+
+    var ops = stockTickerData();
+    ops.setCallbacks(setMarketState, assignStocks, replaceStock);
+    ops.initializeClient();
+}
---
+++ AngularJSImplementation/factory.js
@@ -0,0 +1,95 @@
+﻿var app = angular.module('app', []);
+app.value('$', $);
+app.factory('stockTickerData', ['$', '$rootScope', function ($, $rootScope) {
+    function stockTickerOperations() {
+        //Objects needed for SignalR
+        var connection;
+        var proxy;
+
+        //To set values to fields in the controller
+        var setMarketState;
+        var setValues;
+        var updateStocks;
+
+        var setCallbacks = function (setMarketStateCallback, setValuesCallback, updateStocksCallback) {
+            setMarketState = setMarketStateCallback;
+            setValues = setValuesCallback;
+            updateStocks = updateStocksCallback;
+        };
+
+        var initializeClient = function () {
+            //Creating connection and proxy objects
+            connection = $.hubConnection();
+            proxy = connection.createHubProxy('stockTicker');
+
+            configureProxyClientFunctions();
+
+            start();
+        };
+
+        var configureProxyClientFunctions = function () {
+            proxy.on('marketOpened', function () {
+                //set market state as open
+                $rootScope.$apply(setMarketState(true));
+            });
+
+            proxy.on('marketClosed', function () {
+                //set market state as closed
+                $rootScope.$apply(setMarketState(false));
+            });
+
+            proxy.on('marketReset', function () {
+                //Reset stock values
+                initializeStockMarket();
+            });
+
+            proxy.on('updateStockPrice', function (stock) {
+                $rootScope.$apply(updateStocks(stock));
+            });
+        };
+
+        var initializeStockMarket = function () {
+            //Getting values of stocks from the hub and setting it to controller's field
+            proxy.invoke('getAllStocks').done(function (data) {
+                $rootScope.$apply(setValues(data));
+            }).pipe(function () {
+                //Setting market state to field in controller based on the current state
+                proxy.invoke('getMarketState').done(function (state) {
+                    if (state == 'Open')
+                        $rootScope.$apply(setMarketState(true));
+                    else
+                        $rootScope.$apply(setMarketState(false));
+                });
+            });
+        };
+
+        var start = function () {
+            //Starting the connection and initializing market
+            connection.start().pipe(function () {
+                initializeStockMarket();
+            });
+        };
+
+        var openMarket = function () {
+            proxy.invoke('openMarket');
+        };
+
+        var closeMarket = function () {
+            proxy.invoke('closeMarket');
+        };
+
+        var reset = function () {
+            proxy.invoke('reset');
+        };
+
+        return {
+            initializeClient: initializeClient,
+            openMarket: openMarket,
+            closeMarket: closeMarket,
+            reset: reset,
+            setCallbacks: setCallbacks
+        }
+    };
+
+    return stockTickerOperations;
+} ]);
