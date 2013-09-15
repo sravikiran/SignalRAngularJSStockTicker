a1db8255234e4f0ecc450b4bd086dfcfd3a89d5f
S Ravi Kiran <yuvakiran2009@gmail.com>
2013-09-16 01:13:18 +0530

    Created a separate controller and service
    The new controller and service implement the logic of SignalR
    communication in a better way, using $rootScope.$emit and $rootScope.$on

---
+++ AngularJSImplementation/controller.02.js
@@ -0,0 +1,61 @@
+﻿var StockTickerCtrl = function ($scope, stockTickerData) {
+
+    $scope.stocks = [];
+    $scope.marketIsOpen = false;
+
+    $scope.openMarket = function () {
+        stockTickerData.openMarket();
+    }
+
+    $scope.closeMarket = function () {
+        stockTickerData.closeMarket();
+    }
+
+    $scope.reset = function () {
+        stockTickerData.reset();
+    }
+
+    function assignStocks(stocks) {
+        $scope.stocks = stocks;
+    }
+
+    function replaceStock(stock) {
+        for (var count = 0; count < $scope.stocks.length; count++) {
+            if ($scope.stocks[count].Symbol == stock.Symbol) {
+                $scope.stocks[count] = stock;
+            }
+        }
+    }
+
+    function setMarketState(isOpen) {
+        $scope.marketIsOpen = isOpen;
+    }
+
+    //    var ops = stockTickerData();
+    //    ops.setCallbacks(setMarketState, assignStocks, replaceStock);
+    stockTickerData.initializeClient();
+
+    $scope.$parent.$on('marketOpened', function (e, status) {
+        $scope.$apply(function () {
+            setMarketState(status);
+        });
+    });
+
+    $scope.$parent.$on('marketClosed', function (e, status) {
+        $scope.$apply(function () {
+            setMarketState(!status);
+        });
+    });
+
+    $scope.$parent.$on('updateStock', function (e, stock) {
+        $scope.$apply(function () {
+            replaceStock(stock);
+        });
+    });
+
+    $scope.$parent.$on('setStocks', function (e, stocks) {
+        $scope.$apply(function () {
+            assignStocks(stocks);
+        });
+    });
+}
---
+++ AngularJSImplementation/service.js
@@ -0,0 +1,94 @@
+﻿var app = angular.module('app', []);
+app.value('$', $);
+app.service('stockTickerData', ['$', '$rootScope', function ($, $rootScope) {
+
+    //Objects needed for SignalR
+    var connection;
+    var proxy;
+
+    //To set values to fields in the controller
+    var setMarketState;
+    var setValues;
+    var updateStocks;
+
+    var initializeClient = function () {
+        //Creating connection and proxy objects
+        connection = $.hubConnection();
+        proxy = connection.createHubProxy('stockTicker');
+
+        configureProxyClientFunctions();
+
+        start();
+    };
+
+    var configureProxyClientFunctions = function () {
+        proxy.on('marketOpened', function () {
+            //set market state as open
+            //$rootScope.$apply(setMarketState(true));
+            $rootScope.$emit('marketOpened', true);
+        });
+
+        proxy.on('marketClosed', function () {
+            //set market state as closed
+            //$rootScope.$apply(setMarketState(false));
+            $rootScope.$emit('marketClosed', true);
+        });
+
+        proxy.on('marketReset', function () {
+            //Reset stock values
+            initializeStockMarket();
+        });
+
+        proxy.on('updateStockPrice', function (stock) {
+            //$rootScope.$apply(updateStocks(stock));
+            $rootScope.$emit('updateStock', stock);
+        });
+    };
+
+    var initializeStockMarket = function () {
+        //Getting values of stocks from the hub and setting it to controller's field
+        proxy.invoke('getAllStocks').done(function (data) {
+            //$rootScope.$apply(setValues(data));
+            $rootScope.$emit('setStocks', data);
+        }).pipe(function () {
+            //Setting market state to field in controller based on the current state
+            proxy.invoke('getMarketState').done(function (state) {
+                if (state == 'Open') {
+                    //$rootScope.$apply(setMarketState(true));
+                    $rootScope.$emit('marketOpened', true);
+                }
+                else {
+                    //$rootScope.$apply(setMarketState(false));
+                    $rootScope.$emit('marketClosed', true);
+                }
+            });
+        });
+    };
+
+    var start = function () {
+        //Starting the connection and initializing market
+        connection.start().pipe(function () {
+            initializeStockMarket();
+        });
+    };
+
+    var openMarket = function () {
+        proxy.invoke('openMarket');
+    };
+
+    var closeMarket = function () {
+        proxy.invoke('closeMarket');
+    };
+
+    var reset = function () {
+        proxy.invoke('reset');
+    };
+
+    return {
+        initializeClient: initializeClient,
+        openMarket: openMarket,
+        closeMarket: closeMarket,
+        reset: reset
+    };
+}
+]);
